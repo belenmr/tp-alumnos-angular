7bc87e5d0baab17b2180aeaf0330f8da08c0d114
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-18 20:35:33 -0300

    Se modifica modelo alumno

--- src/app/models/student.model.ts
+++ src/app/models/student.model.ts
@@ -8,17 +8,4 @@ export class Student{
     status: Status;
     firstQualification: number;
     secondQualification: number;
-
-    alumnoList: Student[];
-
-
-    constructor(docketParam:number, nameParam:string, surnameParam:string, dniParam:number, statusParam: Status){
-        this.docket = docketParam;
-        this.name = nameParam;
-        this.surname = surnameParam;
-        this.dni = dniParam;
-        this.status = statusParam;
-
-        this.alumnoList.push(this);
-    }
 }
