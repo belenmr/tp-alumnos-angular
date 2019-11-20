f6f6b6dd66a79eaf93799116c172063ff710425e
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-19 21:29:03 -0300

    Se modifica componente estado

--- src/app/components/status/status.component.ts
+++ src/app/components/status/status.component.ts
@@ -1,15 +1,87 @@
+import { Status } from '../../models/status.model' 
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-status',
-  templateUrl: './status.component.html',
-  styleUrls: ['./status.component.css']
+  	selector: 'app-status',
+  	templateUrl: './status.component.html',
+  	styleUrls: ['./status.component.css']
 })
 export class StatusComponent implements OnInit {
 
-  constructor() { }
+  	statusList: Status[] = [
+		  {code: "R", name: "Regular"}
+	  ];
 
-  ngOnInit() {
-  }
+  	selectedStatus: Status = new Status();
+
+  	addOrEditStatus(){
+		if (!this.existStatus(this.selectedStatus)) {
+			if(this.existStatusCode(this.selectedStatus)){
+				alert("Codigo ya en uso. Intente otro codigo");
+			} else {
+				if (this.existStatusName(this.selectedStatus)) {
+					alert("Nombre de estado ya en uso. Intente otro nombre");
+				} else {
+					this.statusList.push(this.selectedStatus);
+				}
+			}
+		}
+
+		this.selectedStatus = new Status();
+	}
+
+	openEdition(status: Status){
+		this.selectedStatus = status;
+	}
+	  
+	deleteStatus(){
+		if (confirm("¿Quiere eliminarlo?")) {
+			this.statusList = this.statusList.filter(x => x!= this.selectedStatus);
+			this.selectedStatus = new Status();
+		}
+	}
+
+	existStatus(status: Status):boolean{
+		let exist: boolean = false;
+
+		this.statusList.forEach(element => {
+			if (element.name == status.name && element.code == status.code) {
+				exist = true;
+			}
+		});
+		
+		return exist;
+	}
+
+	existStatusName(status: Status):boolean{
+		let exist: boolean = false;
+
+		this.statusList.forEach(element => {
+			if (element.name == status.name) {
+				exist = true;
+			}
+		});
+
+		return exist;
+	}
+
+	existStatusCode(status: Status):boolean{
+		let exist: boolean = false;
+
+		this.statusList.forEach(element => {
+			if (element.code == status.code) {
+				exist = true;
+			}
+		});
+
+		return exist;
+	}
+
+	cancelEditStatus(){
+		this.selectedStatus = new Status();
+	}
+  
+  	ngOnInit() {
+	}
 
 }
