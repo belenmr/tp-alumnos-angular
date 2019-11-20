861d1660aa3b36358bd391eafcd948c084e85b51
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-19 21:17:25 -0300

    Se agrega routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './components/student/student.component';
+import { StatusComponent } from './components/status/status.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'alumnos', component: StudentComponent},
+  {path:'estados', component: StatusComponent},
+  {path: '**', pathMatch:'full', redirectTo:'alumnos'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
