b223001607325a76dc8897225dda422323369e3c
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-23 16:30:11 -0300

    Se agregan router links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StudentComponent } from './components/student/student.component';
 import { StatusComponent } from './components/status/status.component';
+import { QualificationComponent } from './components/qualification/qualification.component';
+import { AverageComponent } from './components/average/average.component';
+import { DisapprovedComponent } from './components/disapproved/disapproved.component';
+import { UserManualComponent } from './components/user-manual/user-manual.component';
 
 
 const routes: Routes = [
   {path: 'alumnos', component: StudentComponent},
   {path:'estados', component: StatusComponent},
+  {path: 'notas', component: QualificationComponent},
+  {path: 'reporte-promedios', component: AverageComponent},
+  {path: 'reporte-desaprobados', component: DisapprovedComponent},
+  {path: 'manual-usuario', component: UserManualComponent}
   {path: '**', pathMatch:'full', redirectTo:'alumnos'}
 ];
 
