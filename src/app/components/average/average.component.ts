1cd388cfdeeb6063807fa48ebda0a0addb5fb7ab
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-25 00:58:31 -0300

    Se modifica componente reporte de promedios

--- src/app/components/average/average.component.ts
+++ src/app/components/average/average.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/models/student.model';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
-  selector: 'app-average',
-  templateUrl: './average.component.html',
-  styleUrls: ['./average.component.css']
+	selector: 'app-average',
+	templateUrl: './average.component.html',
+	styleUrls: ['./average.component.css']
 })
 export class AverageComponent implements OnInit {
 
-  constructor() { }
+	private studentsList: Student[];
+	private storage: LocalStorageService= new LocalStorageService();
 
-  ngOnInit() {
-  }
+	constructor() {
+		this.studentsList = this.storage.getStudentsList();
+	}
+
+	ngOnInit() {
+	}
 
 }
