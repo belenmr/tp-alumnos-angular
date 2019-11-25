2b7bbc7c2e9e22b2124558ebee5dec276333eaaf
Belen Medina Roldan <bmedinaroldan@gmail.com>
2019-11-25 00:58:57 -0300

    Se modifica componente reporte de desaprobados

--- src/app/components/disapproved/disapproved.component.ts
+++ src/app/components/disapproved/disapproved.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/models/student.model';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+
+const minQualification: number = 4;
 
 @Component({
-  selector: 'app-disapproved',
-  templateUrl: './disapproved.component.html',
-  styleUrls: ['./disapproved.component.css']
+  	selector: 'app-disapproved',
+  	templateUrl: './disapproved.component.html',
+  	styleUrls: ['./disapproved.component.css']
 })
+
+
 export class DisapprovedComponent implements OnInit {
 
-  constructor() { }
+	private studentsList: Student[];
+	private DisapprovedStudentsList: Student[];
+	private storage: LocalStorageService= new LocalStorageService(); 
+	
 
-  ngOnInit() {
-  }
+  	constructor() {
+		this.studentsList = this.storage.getStudentsList();
+		this.DisapprovedStudentsList = this.loadDisapprovedList(this.studentsList);
+
+		if (!this.existDisapproved(this.DisapprovedStudentsList)) {
+			alert("No hay alumnos desaprobados");
+		}
+	}
+
+	loadDisapprovedList(list: Student[]): Student[]{
+		let disapproveds: Student[] = new Array<Student>();
+
+		list.forEach(element => {
+			if (element.average < minQualification) {
+				disapproveds.push(element);
+			}
+		});
+
+		return disapproveds;
+	}
+
+
+	existDisapproved(list: Student[]):boolean{
+		let exist: boolean = true;
+		let size: number = list.length;
+
+		if (size == 0) {
+			exist = false;
+		}
+		
+		return exist;
+	}
+
+
+
+  	ngOnInit() {
+  	}
 
 }
